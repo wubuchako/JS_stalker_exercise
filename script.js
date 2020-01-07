f5bcf4d4dca20751012f076c607047d5a20e410c
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-01-07 10:38:29 +0900

    JS_stlaker_exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your firstname ? ");
+var lastName = prompt("What is your lastname?");
+var userAge = prompt("What is your age ? ");
+
+var fullName = firstName +  " " + lastName
+
+console.log("Your fullname is " + fullName);
+
+console.log("You are " + userAge + " years old." ); 
